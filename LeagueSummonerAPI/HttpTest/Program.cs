250cc6b225b270f129f17a6cb9964d32c4348fd6
genious97 <robbiep1@hotmail.com>
2018-07-04 19:28:43 -0400

    added a test for the http get

---
+++ LeagueSummonerAPI/HttpTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+
+namespace HttpTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string apiKey = "";
+			string target = "genious97";
+			string url = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/";
+			Console.Write(Get(url + target + "?api_key=" + apiKey));
+			Console.ReadLine();
+
+		}
+
+		public static string Get(string uri)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+			using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+			using (Stream stream = response.GetResponseStream())
+			using(StreamReader reader = new StreamReader(stream))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
---
+++ LeagueSummonerAPI/Summoner.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Summoner
+{
+	public int profileIconId { get; set; }
+	public string name { get; set; }
+	public long summonerLevel { get; set; }
+	public long revisionDate { get; set; }
+	public long id { get; set; }
+}
