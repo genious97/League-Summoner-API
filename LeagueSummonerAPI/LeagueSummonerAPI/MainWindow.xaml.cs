288bbbae45213dffe2e28f022a6498985a5ff612
genious97 <robbiep1@hotmail.com>
2018-07-04 18:41:06 -0400

    Refactored

--- LeagueSummonerAPI/LeagueSummonerAPI/MainWindow.xaml.cs
+++ LeagueSummonerAPI/LeagueSummonerAPI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace User_Interface
+namespace LeagueSummonerAPI
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
--- LeagueSummonerAPI/LeagueSummonerAPI/Properties/Settings.Designer.cs
+++ LeagueSummonerAPI/LeagueSummonerAPI/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace User_Interface.Properties
+namespace LeagueSummonerAPI.Properties
 {
 
 
